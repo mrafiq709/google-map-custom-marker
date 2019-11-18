89178fd3706736e22e500385b7d759460d2d141e
mrafiq709 <mrafiq709@gmail.com>
2019-11-18 17:13:34 +0700

    must enable billing for use cloud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,175 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+      * element that contains the map. */
+      #map {
+        height: 100%;
+    }
+    /* Optional: Makes the sample page fill the window. */
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+</style>
+</head>
+<body>
+    <!-- Add an input button to initiate the toggle method on the overlay. -->
+    <div id="floating-panel">
+        <input type="button" value="Toggle visibility" onclick="overlay.toggle();"></input>
+        <input type="button" value="Toggle DOM attachment" onclick="overlay.toggleDOM();"></input>
+    </div>
+    <div id="map"></div>
+    <script>
+        // This example creates a custom overlay called USGSOverlay, containing
+        // a U.S. Geological Survey (USGS) image of the relevant area on the map.
 
-        <title>Laravel</title>
+        // Set the custom overlay object's prototype to a new instance
+        // of OverlayView. In effect, this will subclass the overlay class therefore
+        // it's simpler to load the API synchronously, using
+        // google.maps.event.addDomListener().
+        // Note that we set the prototype to an instance, rather than the
+        // parent class itself, because we do not wish to modify the parent class.
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        var overlay;
+        // USGSOverlay.prototype = new google.maps.OverlayView();
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+        // Initialize the map and the custom overlay.
+
+        function initMap() {
+            USGSOverlay.prototype = new google.maps.OverlayView();
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 11,
+                center: {lat: 62.323907, lng: -150.109291},
+                mapTypeId: 'satellite'
+            });
+
+            var bounds = new google.maps.LatLngBounds(
+              new google.maps.LatLng(62.281819, -150.287132),
+              new google.maps.LatLng(62.400471, -150.005608));
+
+            // The photograph is courtesy of the U.S. Geological Survey.
+            var srcImage = 'https://developers.google.com/maps/documentation/' +
+            'javascript/examples/full/images/talkeetna.png';
+
+            // The custom USGSOverlay object contains the USGS image,
+            // the bounds of the image, and a reference to the map.
+            overlay = new USGSOverlay(bounds, srcImage, map);
+        }
+
+        /** @constructor */
+        function USGSOverlay(bounds, image, map) {
+
+            // Initialize all properties.
+            this.bounds_ = bounds;
+            this.image_ = image;
+            this.map_ = map;
+
+            // Define a property to hold the image's div. We'll
+            // actually create this div upon receipt of the onAdd()
+            // method so we'll leave it null for now.
+            this.div_ = null;
+
+            // Explicitly call setMap on this overlay.
+            this.setMap(map);
+        }
+
+        /**
+        * onAdd is called when the map's panes are ready and the overlay has been
+        * added to the map.
+        */
+        USGSOverlay.prototype.onAdd = function() {
+
+            var div = document.createElement('div');
+            div.style.borderStyle = 'none';
+            div.style.borderWidth = '0px';
+            div.style.position = 'absolute';
+
+            // Create the img element and attach it to the div.
+            var img = document.createElement('img');
+            img.src = this.image_;
+            img.style.width = '100%';
+            img.style.height = '100%';
+            img.style.position = 'absolute';
+            div.appendChild(img);
+
+            this.div_ = div;
+
+            // Add the element to the "overlayLayer" pane.
+            var panes = this.getPanes();
+            panes.overlayLayer.appendChild(div);
+        };
+
+        USGSOverlay.prototype.draw = function() {
+
+            // We use the south-west and north-east
+            // coordinates of the overlay to peg it to the correct position and size.
+            // To do this, we need to retrieve the projection from the overlay.
+            var overlayProjection = this.getProjection();
+
+            // Retrieve the south-west and north-east coordinates of this overlay
+            // in LatLngs and convert them to pixel coordinates.
+            // We'll use these coordinates to resize the div.
+            var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+            var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+            // Resize the image's div to fit the indicated dimensions.
+            var div = this.div_;
+            div.style.left = sw.x + 'px';
+            div.style.top = ne.y + 'px';
+            div.style.width = (ne.x - sw.x) + 'px';
+            div.style.height = (sw.y - ne.y) + 'px';
+        };
+
+        // The onRemove() method will be called automatically from the API if
+        // we ever set the overlay's map property to 'null'.
+        USGSOverlay.prototype.onRemove = function() {
+            this.div_.parentNode.removeChild(this.div_);
+            this.div_ = null;
+        };
+
+        // Set the visibility to 'hidden' or 'visible'.
+        USGSOverlay.prototype.hide = function() {
+            if (this.div_) {
+                // The visibility property must be a string enclosed in quotes.
+                this.div_.style.visibility = 'hidden';
             }
+        };
 
-            .full-height {
-                height: 100vh;
-            }
+        USGSOverlay.prototype.show = function() {
+          if (this.div_) {
+            this.div_.style.visibility = 'visible';
+        }
+    };
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    USGSOverlay.prototype.toggle = function() {
+      if (this.div_) {
+        if (this.div_.style.visibility === 'hidden') {
+          this.show();
+      } else {
+          this.hide();
+      }
+  }
+};
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+// Detach the map from the DOM via toggleDOM().
+// Note that if we later reattach the map, it will be visible again,
+// because the containing <div> is recreated in the overlay's onAdd() method.
+USGSOverlay.prototype.toggleDOM = function() {
+  if (this.getMap()) {
+    // Note: setMap(null) calls OverlayView.onRemove()
+    this.setMap(null);
+} else {
+    this.setMap(this.map_);
+}
+};
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHpPBGr4o-HSytpq2OdhtYKGjH4JLh-bA&callback=initMap"
+async defer></script>
+</body>
 </html>
