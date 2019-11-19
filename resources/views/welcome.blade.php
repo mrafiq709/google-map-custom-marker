be40175d31ddf7f991fe076284dd7cb83e997e98
mrafiq709 <mrafiq709@gmail.com>
2019-11-19 17:22:03 +0700

    small and big marker design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,9 +41,42 @@
         display: block;
         width: 0;
     }
+
+    .bigMarker {
+        position:absolute;
+        cursor:pointer;
+        background:#257BC1;
+        width:100px;
+        height:100px;
+        /* -width/2 */
+        margin-left:-79px;
+        /* -height + arrow */
+        margin-top:-170px;
+        border-radius:5px;
+        padding:0px;
+    }
+
+    .bigMarker:after {
+        content:"";
+        position: absolute;
+        bottom: -10px;
+        left: 39px;
+        border-width: 10px 10px 0;
+        border-style: solid;
+        border-color: #257BC1 transparent;
+        display: block;
+        width: 0;
+    }
     .customMarker img {
         width:36px;
         height:35px;
+        margin:2px;
+        border-radius:5px;
+    }
+
+    .bigMarker img {
+        width:96px;
+        height:96px;
         margin:2px;
         border-radius:5px;
     }
@@ -62,16 +95,17 @@
 
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11,
-          center: {lat: 62.323907, lng: -150.109291},
+          zoom: 17,
+          center: new google.maps.LatLng(37.77088429547992, -122.4135623872337),
           mapTypeId: google.maps.MapTypeId.ROADMAP
       });
 
-        var latlng = new google.maps.LatLng(62.323907, -150.109291);
+        var latlng = new google.maps.LatLng(37.77085, -122.41356);
 
-        // The photograph is courtesy of the U.S. Geological Survey.
-        var srcImage = 'https://fooder-dev-s3.s3-ap-northeast-1.amazonaws.com/';
-        srcImage += 'images/1573100726menu.jpg';
+        // var srcImage = 'https://fooder-dev-s3.s3-ap-northeast-1.amazonaws.com/';
+        // srcImage += 'images/1573100726menu.jpg';
+
+        var srcImage = 'http://placekitten.com/90/90';
 
         overlay = new CustomMarker(latlng, srcImage, map);
     }
@@ -82,12 +116,12 @@
         // Now initialize all properties.
         this.latlng_ = latlng;
         this.image_ = image;
-        this.map_ = map;
+        // this.map_ = map;
 
         // Define a property to hold the image's div. We'll
         // actually create this div upon receipt of the onAdd()
         // method so we'll leave it null for now.
-        this.div_ = null;
+        // this.div_ = null;
 
         // Explicitly call setMap on this overlay
         this.setMap(map);
@@ -102,13 +136,17 @@
         var div = document.createElement('div');
 
         // Create the DIV representing our CustomMarker
-        div.className = "customMarker"
+        div.className = "customMarker";
+        div.id = "customMarkerId";
 
         // Create the img element and attach it to the div.
         var img = document.createElement('img');
         img.src = this.image_;
 
         div.appendChild(img);
+        google.maps.event.addDomListener(div, "click", function (event) {
+            google.maps.event.trigger(clickMe(), "click");
+        });
 
         this.div_ = div;
 
@@ -131,15 +169,45 @@
     };
 
     CustomMarker.prototype.onRemove = function() {
-        this.div_.parentNode.removeChild(this.div_);
+        if (this.div_){
+            this.div_.parentNode.removeChild(this.div_);
+            this.div_ = null;
+        }
     };
 
     CustomMarker.prototype.getPosition = function () {
         return this.latlng_;
     };
 
-      google.maps.event.addDomListener(window, 'load', initMap);
-  </script>
+    var toggle = 0;
+
+    function clickMe(){
+        // var x = document.getElementsByClassName("customMarker")[0].className;
+        // console.log('className: ' + x);
+
+        if (!toggle) {
+            // Create dynamic class for big marker
+            document.getElementById("customMarkerId").classList.add('bigMarker');
+
+            // Big Marker Styles
+            var customBigMarker = document.getElementsByClassName("bigMarker")[0];
+            customBigMarker.style.backgroundColor = '#257BC1';
+
+        }else{
+            // Remove Big Marker Class
+            document.getElementById("customMarkerId").classList.remove('bigMarker');
+
+            // Small Marker Styles
+            var customSmallMarker = document.getElementsByClassName("customMarker")[0];
+            customSmallMarker.style.backgroundColor = 'black';
+        }
+        
+        toggle++;
+        toggle %= 2;
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+</script>
 </head>
 <body>
   <div id="map"></div>
